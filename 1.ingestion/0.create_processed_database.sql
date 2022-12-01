1f09fb629be71ace73bfb7edf4db4d4cbd0b6b4d
johnwickhoneybadger@gmail.com <johnwickhoneybadger@gmail.com>
2022-12-01 22:37:08 +0000

    add the notebook for creating processed database

---
+++ 1.ingestion/0.create_processed_database.sql
@@ -0,0 +1,11 @@
+-- Databricks notebook source
+CREATE DATABASE IF NOT EXISTS f1_processed
+LOCATION "/mnt/formulaf1jhdl/processed"
+
+-- COMMAND ----------
+
+DESC DATABASE f1_processed
+
+-- COMMAND ----------
+
+
